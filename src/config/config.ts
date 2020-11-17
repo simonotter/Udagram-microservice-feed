1fd958e0553ef878740bcc95ee7467255fce23d7
Simon Otter <simonotter@gmail.com>
2020-11-17 18:25:14 +0000

    Moved server port to env variable

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,6 +8,7 @@ export const config = {
   'aws_profile': process.env.AWS_PROFILE,
   'aws_media_bucket': process.env.AWS_BUCKET,
   'url': process.env.URL,
+  'port': process.env.PORT,
   'jwt': {
     'secret': process.env.JWT_SECRET,
   },
